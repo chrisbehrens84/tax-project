ae0393be76a304f2f80822d282af82457ee1a2c8
chrisbehrens84 <cbehrens@skillstorm.com>
2023-07-19 20:05:17 -0700

    initial java backend project setup

---
+++ taxapp-backend/src/main/java/com/skillstorm/taxappbackend/TaxappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.skillstorm.taxappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxappBackendApplication.class, args);
+	}
+
+}
---
+++ taxapp-backend/src/test/java/com/skillstorm/taxappbackend/TaxappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillstorm.taxappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
