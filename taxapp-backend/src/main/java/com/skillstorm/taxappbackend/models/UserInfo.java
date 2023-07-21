eb45a81964f336075f4c243a86353943ca036050
chrisbehrens84 <cbehrens@skillstorm.com>
2023-07-21 14:55:03 -0700

    changed column name back

--- taxapp-backend/src/main/java/com/skillstorm/taxappbackend/models/UserInfo.java
+++ taxapp-backend/src/main/java/com/skillstorm/taxappbackend/models/UserInfo.java
@@ -22,11 +22,10 @@ public class UserInfo {
     @GeneratedValue(strategy = GenerationType.IDENTITY) // SPECIFY AUTO INCREMENT
     private Long userInfoId;
 
-   
-    @Column(name = "firstName")
+    @Column(name = "first_name")
     private String firstName;
 
-    @Column(name = "lastName")
+    @Column(name = "last_name")
     private String lastName;
 
     @Column(name = "address")
