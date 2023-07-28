cff2b5fa502bcc205069b9b2cc8e906fa985f790
chrisbehrens84 <cbehrens@skillstorm.com>
2023-07-27 20:53:59 -0700

    refactoring

--- taxapp-backend/src/main/java/com/skillstorm/taxappbackend/models/TaxCalculations.java
+++ taxapp-backend/src/main/java/com/skillstorm/taxappbackend/models/TaxCalculations.java
@@ -1,5 +1,5 @@
 package com.skillstorm.taxappbackend.models;
 
-public class TaxCalcualtion {
+public class TaxCalculations {
   
 }
--- taxapp-backend/src/main/java/com/skillstorm/taxappbackend/repositories/TaxInformationsRepository.java
+++
@@ -1,5 +0,0 @@
-package com.skillstorm.taxappbackend.repositories;
-
-public class TaxInformationsRepository {
-  
-}
