0741ef7c9aae995ea86103a823acb05766bc94d2
chrisbehrens84 <cbehrens@skillstorm.com>
2023-07-27 20:44:44 -0700

    refactoring all code

--- taxapp-backend/src/main/java/com/skillstorm/taxappbackend/controllers/TaxCalculations.java
+++ taxapp-backend/src/main/java/com/skillstorm/taxappbackend/controllers/TaxCalculations.java
@@ -1,5 +1,5 @@
 package com.skillstorm.taxappbackend.controllers;
 
-public class AppUserController {
+public class TaxCalculations {
   
 }
--- taxapp-backend/src/main/java/com/skillstorm/taxappbackend/models/TaxCalcualtion.java
+++ taxapp-backend/src/main/java/com/skillstorm/taxappbackend/models/TaxCalcualtion.java
@@ -1,5 +1,5 @@
 package com.skillstorm.taxappbackend.models;
 
-public class TaxInformation {
+public class TaxCalcualtion {
   
 }
